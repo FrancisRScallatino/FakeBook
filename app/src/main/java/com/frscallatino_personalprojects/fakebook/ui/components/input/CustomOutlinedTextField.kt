a27cde6b3925558158f1c8fe925549fa65d18731
Francis R Scallatino <FrancisRScallatino@gmail.com>
2024-04-29 07:54:15 -0400

    add preview

--- app/src/main/java/com/frscallatino_personalprojects/fakebook/ui/components/input/CustomOutlinedTextField.kt
+++ app/src/main/java/com/frscallatino_personalprojects/fakebook/ui/components/input/CustomOutlinedTextField.kt
@@ -7,11 +7,18 @@ import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.frscallatino_personalprojects.fakebook.ui.theme.White5
 
@@ -43,5 +50,19 @@ fun CustomOutlinedTextField(
         leadingIcon = {
             Icon(imageVector = icon, contentDescription = "")
         }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCustomOutlinedTextField(){
+
+    var usernameInput by remember { mutableStateOf("") }
+
+    CustomOutlinedTextField(
+        value = usernameInput,
+        label = "username",
+        icon = Icons.Default.AccountCircle,
+        onValueChange = { usernameInput = it }
     )
 }
