cdf66c53880730179fb4bce516af7031d349edeb
Francis R Scallatino <FrancisRScallatino@gmail.com>
2024-04-29 07:16:57 -0400

    update dependencies for UI components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // UI
+    implementation("androidx.compose.material:material:1.6.6")
+    implementation("androidx.compose.material:material-icons-extended:1.6.6")
 }
